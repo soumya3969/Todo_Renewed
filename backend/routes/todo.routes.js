bc806614ec995b4b2916c26c5c3416f412b288f7
soumya3969 <srmhnt2001@gmail.com>
2024-11-13 16:04:39 +0530

    Fix import statement for todo model and add CRUD routes for Todo operations

--- backend/routes/todo.routes.js
+++ backend/routes/todo.routes.js
@@ -9,6 +9,12 @@ import {
   checkAuth
 } from "../controllers/auth.controller.js";
 import { verifyToken } from "../middleware/verifyToken.js";
+import {
+  createTodo,
+  getTodos,
+  updateTodo,
+  deleteTodo
+} from "../controllers/todo.controller.js";
 
 const router = express.Router();
 
@@ -23,4 +29,9 @@ router.post("/forgot-password", forgotPassword);
 
 router.post("/reset-password/:token", resetPassword);
 
+// *todo routes
+router.post("/create-todo", verifyToken, createTodo);
+router.get("/get-todos", verifyToken, getTodos);
+router.put("/update-todo/:id", verifyToken, updateTodo);
+router.delete("/delete-todo/:id", verifyToken, deleteTodo);
 export default router;
